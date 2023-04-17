d4ec373e26cbb44b92552d3c2ef44e3f37be8bf7
henry-hugo <henrique.hugo.9@hotmail.com>
2023-04-17 02:21:22 -0300

    atualizaçaoParaXML
    espero que considere a troca do array direto no codigo, pois n tinha visto a mensagem com os dados .

--- TesteVaga/script2.js
+++ TesteVaga/script2.js
@@ -1,32 +1,39 @@
-const faturamentoDiario = [50, 60, 70, 0, 80, 90, 100, 0, 110, 120, 130, 140, 150, 160, 170, 180, 0, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300];
+// Carrega o arquivo XML
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "dados.xml", false);
+xmlhttp.send();
+const xmlDoc = xmlhttp.responseXML;
 
-var menorFaturamento = faturamentoDiario[0];
-var maiorFaturamento = faturamentoDiario[0];
-var somaFaturamento = 0;
-var diasComFaturamentoAcimaDaMedia = 0;
+// Obtém a lista de dias e valores
+const dias = xmlDoc.getElementsByTagName("dia");
+const valores = xmlDoc.getElementsByTagName("valor");
 
-for (let i = 0; i < faturamentoDiario.length; i++) {
-  if (faturamentoDiario[i] < menorFaturamento) {
-    menorFaturamento = faturamentoDiario[i];
-  }
+// Converte os valores em números e armazena em um array
+const valoresNum = [];
+for (let i = 0; i < valores.length; i++) {
+  valoresNum.push(parseFloat(valores[i].childNodes[0].nodeValue));
+}
 
-  if (faturamentoDiario[i] > maiorFaturamento) {
-    maiorFaturamento = faturamentoDiario[i];
-  }
+// Calcula o menor valor de faturamento
+const menorValor = Math.min(...valoresNum);
 
-  if (faturamentoDiario[i] > 0) {
-    somaFaturamento += faturamentoDiario[i];
+// Calcula o maior valor de faturamento
+const maiorValor = Math.max(...valoresNum);
+
+// Calcula a média mensal de faturamento
+const somaValores = valoresNum.reduce((acc, val) => acc + val, 0);
+const mediaMensal = somaValores / valoresNum.length;
+
+// Calcula o número de dias em que o faturamento foi superior à média mensal
+let numDiasAcimaMedia = 0;
+for (let i = 0; i < valoresNum.length; i++) {
+  if (valoresNum[i] > mediaMensal) {
+    numDiasAcimaMedia++;
   }
 }
 
-const mediaMensal = somaFaturamento / faturamentoDiario.filter((valor) => valor > 0).length;
+// Imprime os resultados
+document.write("Menor valor de faturamento: " + menorValor);
+document.write("<br>Maior valor de faturamento: " + maiorValor);
+document.write("<br>Número de dias com faturamento acima da média mensal: " + numDiasAcimaMedia);
 
-for (let i = 0; i < faturamentoDiario.length; i++) {
-  if (faturamentoDiario[i] > mediaMensal) {
-    diasComFaturamentoAcimaDaMedia++;
-  }
-}
-
-document.write(`Menor faturamento: R$${menorFaturamento}`);
-document.write(`<br> Maior faturamento: R$${maiorFaturamento}`);
-document.write(`<br> Dias com faturamento acima da média mensal: ${diasComFaturamentoAcimaDaMedia}`);
